fb184d0f740d5e779ab39f028f3323242fb66fef
daniel-karunya <91770270+daniel-karunya@users.noreply.github.com>
2022-08-11 23:52:36 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 # Write your code here
+flist=[]
 for i in circular_linked_list:
     if i not in flist:
         flist.append(i)
