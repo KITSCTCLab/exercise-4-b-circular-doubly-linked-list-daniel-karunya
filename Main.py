5c83e7940f3f3b24c3e77d0e207828dc78e07a14
daniel-karunya <91770270+daniel-karunya@users.noreply.github.com>
2022-08-11 23:17:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 # Write your code here
-print(circular_linked_list[0]," ",circular_linked_list[1]," ",circular_linked_list[2],ends="")
+print(circular_linked_list[0]," ",circular_linked_list[1]," ",circular_linked_list[2],end="")
 final_list = [circular_linked_list[0],circular_linked_list[1],circular_linked_list[2]]
 for i in range (2,length_of_circular_linked_length):
   if circular_linked_linked[i] not in final_list:
